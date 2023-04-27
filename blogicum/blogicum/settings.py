86e03c4258326a1fc4493eeed5f3fa9bb1a4d51e
Максим Новиков <maksim.nov04@gmail.com>
2023-04-27 21:13:54 +0300

    pep8

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-] 
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
